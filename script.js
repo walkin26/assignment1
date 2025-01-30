cf7d83e74b8c78da37bfb0fff1fe4fcf61b8f5fd
walkin26 <itsjoaquinsolon@gmail.com>
2025-01-30 04:25:06 +0000

    Setup

---
+++ script.js
@@ -0,0 +1,58 @@
+function start() {
+
+    // Bar Chart
+
+    const ctx = document.getElementById('myBarChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Revenues', 'Expenses'],
+            datasets: [{
+                label: 'In millions',
+                data: [444, 421],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    // Pie Chart
+
+    const pieCtx = document.getElementById('myPieChart');
+    new Chart(pieCtx, {
+        type: 'pie',
+        data: {
+            labels: ['English', 'French', 'Other', 'Prefer not to say'],
+            datasets: [{
+                label: '# of responses',
+                data: [44, 2, 51, 3],
+                borderWidth: 1
+            }]
+        }
+    });
+
+
+    // Line Chart
+
+    const lineCtx = document.getElementById('lineChart');
+    new Chart(lineCtx, {
+        type: 'line',
+        data: {
+            labels: ['2008', '2009', '2010', '2011'],
+            datasets: [{
+                label: 'George Brown College',
+                data: [12748, 13866, 13914, 14068],
+                borderWidth: 1
+            }]
+        }
+    });
+
+}
+
+$(window).on('load', start);
